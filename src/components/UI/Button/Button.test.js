9b05528dd99dff5b5f272e6b673b67912e79087a
Eduardo Plaza <eduardoplaza@Eduardos-iMac.local>
2021-10-04 16:29:39 +0200

    click on button test

--- src/components/UI/Button/Button.test.js
+++ src/components/UI/Button/Button.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Button from './index'
 
 it('Should render same text passed into text prop', async () => {
   render(<Button text="My Button Text" />)
   const ButtonElement = screen.getByText(/my button text/i)
   expect(ButtonElement).toBeInTheDocument()
+})
+
+const mockedAddToCart = jest.fn()
+
+it('Should trigger a function when clicked', async () => {
+  render(<Button text="Add to cart" onClick={mockedAddToCart} />)
+  const ButtonElement = screen.getByText(/add to cart/i)
+  fireEvent.click(ButtonElement)
+  expect(mockedAddToCart).toHaveBeenCalled()
 })
